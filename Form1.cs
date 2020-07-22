ee539ce0c321efc2ffa997661cd80f7d5efff9ff
alpaddesai <alpa.gupta@gmail.com>
2020-07-21 21:07:42 -0400

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemsEngineering
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axAcroPDF1_Enter(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+
+            axAcroPDF1.src = file.FileName;
+
+        }
+    }
+}
